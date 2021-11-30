3e5a9d28b9a9e07b8f9d6b1734d4d173f93faf61
anupr567 <54394507+anupr567@users.noreply.github.com>
2021-11-30 19:40:11 +0530

    Delete build_all.sh

--- build_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-cd components
-for container in $(ls -d */ | sed 's%/%%g'); do
-  cd $container
-  echo "Building Docker container in $container"
-  bash ../build_container.sh
-  cd ..
-done
