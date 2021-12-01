d23d39f2e5384f364388930072f55cd88234765c
anupr567 <54394507+anupr567@users.noreply.github.com>
2021-12-01 15:45:43 +0530

    Added pipeline host url

--- kfp_pipeline.py
+++ kfp_pipeline.py
@@ -15,14 +15,15 @@ def first_pipeline():
     # Loads the yaml manifest for components
     extract_data = kfp.components.load_component_from_file('components\extract_data\extract_data.yaml')
     preprocess_data = kfp.components.load_component_from_file('components\preprocess\preprocess_data_classifier.yaml')
-
+    linear_regression = kfp.components.load_component_from_file('components\linear_regression\linear_regression.yaml')
     # Run task
     extract_task = extract_data()
-    preprocess_task = preprocess_data()
+    preprocess_task = preprocess_data(extract_task.output)
+    linear_regression_task = linear_regression(preprocess_task.output)
 
 
 # submit the pipeline for execution.
 if __name__ == '__main__':
     pipeline = kfp.Client(
-        host='https://7a7aa105a36f25a2-dot-us-central1.pipelines.googleusercontent.com').create_run_from_pipeline_func(
-        first_pipeline, arguments={})        # how not to hardcode the host url?
+        host='https://4ccec0259a834495-dot-us-central1.pipelines.googleusercontent.com').create_run_from_pipeline_func(
+        first_pipeline, arguments={})       
