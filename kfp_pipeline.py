ec5d9531e2a15a176c4a354ebb625971c0c8ceda
anupr567 <54394507+anupr567@users.noreply.github.com>
2021-12-02 17:28:11 +0530

    Update kfp_pipeline.py

--- kfp_pipeline.py
+++ kfp_pipeline.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Oct  8 17:11:47 2021
 
-@author: Anup
+@author: Nikhil
 """
 
 import kfp
@@ -13,16 +13,19 @@ from kfp import dsl
               description='Applies extraction of data and pre processing it.')
 def first_pipeline():
     # Loads the yaml manifest for components
-    extract_data = kfp.components.load_component_from_file('components\extract_data\extract_data.yaml')
-    preprocess_data = kfp.components.load_component_from_file('components\preprocess\preprocess_data_classifier.yaml')
-    linear_regression = kfp.components.load_component_from_file('components\linear_regression\linear_regression.yaml')
+    extract_data = kfp.components.load_component_from_file('components/extract_data/extract_data.yaml')
+    preprocess_data = kfp.components.load_component_from_file('components/preprocess/process_data_classifier.yaml')
+    logistic_regression = kfp.components.load_component_from_file('components/logistic_regression/logistic_regression.yaml')
+    random_forest_classifier = kfp.components.load_component_from_file('components/random_forest/random_forest_classifier.yaml')
     # Run task
     extract_task = extract_data()
     preprocess_task = preprocess_data(extract_task.output)
-    linear_regression_task = linear_regression(preprocess_task.output)
+    logistic_regression_task = linear_regression(preprocess_task.output)
+    random_forest_classifier_task = random_forest_classifier(preprocess_task.output)
 
 
 # submit the pipeline for execution.
+# keep in mind to change the host url according to your host.
 if __name__ == '__main__':
     pipeline = kfp.Client(
         host='https://4ccec0259a834495-dot-us-central1.pipelines.googleusercontent.com').create_run_from_pipeline_func(
