d5e9f5502ce6fd24e623e71dc1c020d6b7c0d0bb
anupr567 <54394507+anupr567@users.noreply.github.com>
2021-12-02 17:08:12 +0530

    Update build_container.sh

--- build_container.sh
+++ build_container.sh
@@ -1,24 +1,24 @@
-CONTAINER_NAME=kfp-pipeline-$(basename $(pwd))
+CONTAINER_NAME=kfp-pipeline-$(basename $(pwd))                      # add custom prefix before component name i.e. prefix-component_name, 'kfp-pipeline' is prefix here
 DIR_IN_REPO=$(pwd | sed 's%components/% %g' | awk '{print $2}')     # components/component_name
 REPO_DIR=$(pwd | sed 's%components/%components %g' | awk '{print $1}')    #  ~/repo_name
 
 echo "Creating ${CONTAINER_NAME}:latest from this Dockerfile:"
-cat ${REPO_DIR}/${DIR_IN_REPO}/Dockerfile
+cat ${REPO_DIR}/${DIR_IN_REPO}/Dockerfile                          # ~/repo_name/components/component_name/Dockerfile
 
 
-if [ -z "$1" ]; then
+if [ -z "$1" ]; then                                               # if giving the project ID as first argument then take that else get it
   PROJECT_ID=$(gcloud config config-helper --format "value(configuration.properties.core.project)")
 else
   PROJECT_ID=$1
 fi
 
-if [ -z "$2" ]; then
+if [ -z "$2" ]; then                                               # if giving the tag as second argument then take that else give it "latest"
   TAG_NAME="latest"
 else
   TAG_NAME="$2"
 fi
 
-# Create the container image
+# Create the build configuration file i.e. cloudbuild.yaml
 cat <<EOM > cloudbuild.yaml
 steps:
     - name: 'gcr.io/cloud-builders/docker'
@@ -27,7 +27,7 @@ steps:
 images:
     - 'gcr.io/${PROJECT_ID}/${CONTAINER_NAME}:${TAG_NAME}'
 EOM
-
+# Running build through cloudbuild.yaml file
 # on the manual build, we should not specify dir:, but for github trigger, we need it
 cat cloudbuild.yaml | grep -v "remove-for-manual" > /tmp/$$
 cat /tmp/$$
