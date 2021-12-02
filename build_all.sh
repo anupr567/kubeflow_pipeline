12d90578938141a891e3b32bdfed4686ce2a8b98
anupr567 <54394507+anupr567@users.noreply.github.com>
2021-12-02 17:03:48 +0530

    Update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
 cd ./components
-for container in $(ls -d */ | sed 's%/%%g'); do
-  cd $container
-  echo "Building Docker container in $container"
+for component_dir in $(ls -d */ | sed 's%/%%g'); do       # going into each component directory
+  cd $component_dir
+  echo "Building Docker container in $component_dir"
   bash ../../build_container.sh
   cd ..
 done
